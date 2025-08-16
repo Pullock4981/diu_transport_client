fc29485ce8856a6da532589e32daa85f2c83692d
asif938 <asif1962000@gmail.com>
2025-08-16 09:19:58 +0600

    Added Bus management page

---
+++ src/Pages/Dashboard/BusManagement/BusManagement.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BusManagement = () => {
+    return (
+        <div>
+            <h1>Bus management</h1>
+        </div>
+    );
+};
+
+export default BusManagement;
